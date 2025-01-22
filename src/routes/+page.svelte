a4ae657ed072b5d9f48a1659ace39f0dbf320ed7
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-01-22 18:00:35 -0400

    alteração do site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
+<div class="geral">
+    <div class="sla">
+        <h1>Se quizer sim mano</h1>
+    </div>
+
+</div>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    div.geral{
+        background-color: rgb(80, 80, 80);
+    }
+    div.sla{
+        background-color: rgb(164, 167, 170);
+        width: 100%;
+        height: 160px;
+        padding: 0px;
+        margin: 0px;
+        font-family: sans-serif;
+        font-weight: 800;
+    }
+
+
+</style>

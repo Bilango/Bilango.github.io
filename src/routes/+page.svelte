90d886a4e6247c1074241c9cbcda7eb31f468fe8
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-01-29 17:45:14 -0400

    wftsgza

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <body>
     <header>
-        <h1>Seloko num compensa..</h1>
+        <div class="top">
+            <h1>Seloko num compensa..</h1> 
+        </div>
         <nav class="top">
             <ul><li>Oloko</li></ul>
             <ul><li>Seloko</li></ul>
         </nav>
 
     </header>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </body>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 
 <style>
@@ -25,5 +26,12 @@
         margin: 0px;
         border: blue solid 3px;
         border-bottom-left-radius: 15px;
+    }
+    div.top{
+        border-bottom: 3px;
+        border-color: darkgray;
+        font-family: sans-serif;
+        font-weight: 700;
+        padding: 10px 15px 5px 15px;
     }
 </style>

7e59afa9b8545af0434352b277bafe6d6f8393c2
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-01-29 17:36:10 -0400

    wawawaw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,17 @@
-<div class="geral">
-    <div class="sla">
-        <h1>Se quizer sim mano</h1>
-    </div>
-    <div class="s2">
-        <div class="ar">Test link</div>
-        <div class="ar">Area link2</div>
-    </div>
-</div>
-
-
+<body>
+    <header>
+        <h1>Seloko num compensa..</h1>
+    </header>
+</body>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+
 <style>
-    div.geral{
-        background-color: rgb(80, 80, 80);
-    }
-    div.sla{
-        background-color: rgb(164, 167, 170);
-        width: 100%;
-        height: 80px;
+    body{
+        background-color: black;
         padding: 0px;
-        margin: 0px;
-        font-family: sans-serif;
-        font-weight: 800;
-        text-align: center;
-        border-bottom-left-radius: 15px;
-    }
-    div.s2{
-        background-color: rgb(219, 219, 219);
-        width: 0px;
-        margin: 0px;
-    }
-    div.ar{
-        background-color: aliceblue;
-        border: 3px;
-        width: 30px;
-        border-style: solid;
-        border-color: black;
-        padding: 2px;
-        margin: 5px;
-        border-radius: 10px;
-        box-shadow: 3px 3px 8px;
+        justify-content: center;
     }
 </style>

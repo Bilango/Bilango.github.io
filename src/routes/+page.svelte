3932044ba54091bf27cf36dd9c150f4f3675125c
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-01-29 18:25:47 -0400

    htdrhsr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,36 @@
-    <header>
-        <h1>Seloko num compensa..</h1>
-        <nav class="top">
-            <span class="logotipo"><li>SLA</li></span>
-            <ul><li>Oloko</li></ul>
-            <ul><li>Seloko</li></ul>
-        </nav>
-    </header>
-    <sidebar>
-    </sidebar>
-    <main>
-    </main>
-    <footer>
-        <h1>Welcome to SvelteKit</h1>
-        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-    </footer>
+<header>
+    <h1>Seloko num compensa..</h1>
+    <nav class="top">
+        <span class="logotipo"><li>SLA</li></span>
+        <ul>
+            <li>Oloko</li>
+            <li>Seloko</li>
+        </ul>
+    </nav>
+</header>
+<sidebar>
+</sidebar>
+<main>
+</main>
+<footer>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</footer>
 
 
 <style>
-    body{
-        background-color: gray;
-        padding: 0px;
-    }
     header{
         width: 100%;
         background-color: white;
-        padding: 15px 10px 5px 10px;
+        padding: 8px 15px 0px 15px;
         margin: 0px;
         border-bottom-left-radius: 15px;
         box-shadow: 3px 3px 8px;
     }
     nav.top{
         display: flex;
-        padding: 3px;
+        margin-top: 5px;
+        padding: 3px 15px 10px 15px;
         border-top: 1px black solid;
         align-content: center;
         justify-content: space-between;

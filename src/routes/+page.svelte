aa3cda7e012a59e5470724628d3476ae0fb37726
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-02-04 16:00:02 -0400

    afefafe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,26 @@
-<header>
-    <h1>Seloko num compensa..</h1>
-    <nav class="top">
-        <span class="logotipo"><li>SLA</li></span>
-        <ul>
-            <li>Oloko</li>
-            <li>Seloko</li>
-        </ul>
-    </nav>
-</header>
-<sidebar>
-</sidebar>
-<main>
-</main>
-<footer>
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-</footer>
+<head>
+    <tittle>Pág comfirm users</tittle>
+</head>
 
+
+<div class="user">
+    <span>Titulo pag</span>
+    Nome:<iput id="name" type="text">
+    Senha: <input id="pass" type="text">
+</div>
 
 <style>
-    header{
-        width: 100%;
-        background-color: white;
-        padding: 8px 15px 0px 15px;
-        margin: 0px;
-        border-bottom-left-radius: 15px;
-        box-shadow: 3px 3px 8px;
+    div{
+        background-color: rgb(218, 218, 218);
+        width: 200px;
+        height: 160px;
     }
-    nav.top{
-        display: flex;
-        margin-top: 5px;
-        padding: 3px 15px 10px 15px;
-        border-top: 1px black solid;
-        align-content: center;
-        justify-content: space-between;
-    }
-    ul,li{
-        text-decoration: none;
-        list-style: none;
-        display: flex;
-        font-family: sans-serif;
-        justify-content: space-evenly;
-    }
-    span.logotipo{
-        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-        font-weight: 900;
-        font-size: 40px;
+
+    body{
+        background-color: rgb(189, 189, 189);
+        justify-content: center;
         align-items: center;
     }
-    footer{
-        width: 100%;
-        background-color: white;
-        padding: 10px 15px 5px 15px;
-        margin: 0px;
-    }
+
+
 </style>

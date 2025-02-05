979b4d6a8dc242ccc8632800aaeb0cecbfb4c66f
Bilango <vitor.matis@estudante.ifms.edu.br>
2025-02-05 17:32:00 -0400

    fasgrsaedg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <p>Comfirmação de entrada</p>
 <input type="text" placeholder="Nome" id="nome">
 <input type="text" placeholder="Senha" id="senha">
-<button>Comfirmar</button>
+<button><p style="font-size:medium;">Comfirmar</p></button>
 
 
 <style>
@@ -14,8 +14,10 @@
         font-size: 30px;
         border:none;
         border-top-left-radius: 4px;
+        display: grid;
     }
     input:hover{
+        color:white;
         background-color: rgb(63, 77, 97);
         border-bottom: rgb(216, 0, 0) solid 2px;
     }
@@ -23,11 +25,8 @@
     button{
         background-color: rgb(77, 169, 255);
         border: solid 2px rgb(176, 246, 255);
+        border-radius: 15px;
         color:white;
-    }
-    button:hover{
-        background-color: rgb(230, 186, 105);
-        border-color: rgb(223, 0, 0);
-        color:yellow;
+        display: grid;
     }
 </style>

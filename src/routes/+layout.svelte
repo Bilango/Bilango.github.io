42a0bd51a5ddf8706ad850e52c39c35400ed20d0
Bilango <vitor.matias@estudante.ifms.edu.br>
2025-02-11 15:05:11 -0400

    fsefasefafe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,34 @@
 
 <nav>
-    <a href="">Home</a>
-    <a href="">More</a>
-    <a href="">Sobre</a>
+    <a href="src\routes\página1">Home</a>
+    <a href="src\routes\página2">More</a>
+    <a href="src\routes\página3">Sobre</a>
 </nav>
+
+
+<style>
+    nav{
+        background-color: rgb(94, 94, 94);
+        border-bottom-left-radius: 15px;
+        padding: 5px 15px 5px 15px;
+        border:gray 1px solid;
+        display:flex;
+        justify-content: center;
+    }
+    a{
+        text-decoration: none;
+        color:black;
+        font-size: 25px;
+        font-weight: 800;
+        font-family:Georgia, 'Times New Roman', Times, serif;
+        transition: padding 0.4s;
+        transition: 0.4s;
+        margin:0px 10px 0px 10px
+    }
+    a:hover{
+        background-color: darkgray;
+        border: solid white 1px;
+        padding: 2px;
+        font-size: 30px;
+        border-radius: 10px;
+    }
